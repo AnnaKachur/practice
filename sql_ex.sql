3ee102d9c7fb0eb0e6f89d7fccb4da18d7b0bf88
Anna Kachur <kachurann@gmail.com>
2022-02-10 12:47:20 +0200

    add solution for ex 6

--- sql_ex.sql
+++ sql_ex.sql
@@ -1,3 +1,8 @@
+-- Solution for exercise 6
+SELECT DISTINCT maker, speed FROM Laptop 
+INNER JOIN Product ON Laptop.model = Product.model
+WHERE hd >= 10
+
 -- Solution for exercise 7
 SELECT DISTINCT PC.model, price FROM Product
 INNER JOIN PC ON Product.model = PC.model
