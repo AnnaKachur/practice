8b73b3b05efef1ae06d97da65ca6f3d57b524456
Anna Kachur <annakachur@Annas-Air.Dlink>
2022-02-10 11:22:21 +0200

    add sql_ex.sql file

---
+++ sql_ex.sql
@@ -0,0 +1,12 @@
+-- Solution for exercise 7
+SELECT DISTINCT PC.model, price FROM Product
+INNER JOIN PC ON Product.model = PC.model
+WHERE maker = 'B'
+UNION
+SELECT DISTINCT Laptop.model, price FROM Product
+INNER JOIN Laptop ON Laptop.model = Product.model
+WHERE maker = 'B'
+UNION
+SELECT DISTINCT Printer.model, price FROM Product
+INNER JOIN Printer ON Printer.model = Product.model
+WHERE maker = 'B'
