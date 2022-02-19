d425ddfad8422c915cf27881ee902cc5b975aa93
Anna Kachur <kachurann@gmail.com>
2022-02-19 22:31:29 +0200

    Refactor mc_donalds.c

--- c_lang_exercises/mc_donalds/mc_donalds.c
+++ c_lang_exercises/mc_donalds/mc_donalds.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
 int main(void)
 {
-    printf("# # ### ###\n# # #     #\n### ###   #\n  #   #   #\n  # ###   #\n");
+    printf("# # ### ###\n");
+    printf("# # #     #\n");
+    printf("### ###   #\n");
+    printf("  #   #   #\n");
+    printf("  # ###   #\n");
 }
