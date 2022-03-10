669c3c98b2ab1c0c0c9c7396972453f110ce9e22
Anna Kachur <kachurann@gmail.com>
2022-03-10 14:56:56 +0200

    add raven.c

---
+++ raven/raven.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Once upon a midnight dreary, while I pondered, weak and weary,\n");
+    printf("Over many a quaint and curious volume of forgotten lore—\n");
+    printf("While I nodded, nearly napping, suddenly there came a tapping,\n");
+    printf("As of some one gently rapping, rapping at my chamber door—\n");
+    printf("\"'Tis some visitor,\" I muttered, \"tapping at my chamber door—\n");
+    printf("                Only this and nothing more.\"\n");
+}
