9c807f547079e61c68c434636aad3f508ca06fed
AnnaKachur <kachurann@gmail.com>
2023-01-20 21:13:23 +0100

    Learning operators

---
+++ javascript/learning_operators.js
@@ -0,0 +1,62 @@
+// Operators
+// Math
+console.log("2 + 3 =", 2 + 3);
+console.log("2 - 3 =", 2 - 3);
+
+console.log("6 * 6 =", 6 * 6);
+console.log("20 / 5 =", 20 / 5);
+
+console.log("17 / 5 =", 17 / 5);
+console.log("17 % 5 =", 17 % 5);
+
+console.log("430 % 2 =", 430 % 2);
+console.log("431 % 2 =", 431 % 2);
+
+console.log("3 ** 3 =", 3 ** 3);
+
+// Assignment
+var x;
+x = 2;
+x = x + 3;
+x += 3;
+// x -= 3;
+// x *= 3;
+// x /= 3;
+// x %= 3;
+// x **= 3;
+
+x = x + 1;
+x += 1;
+x++;
+// x--;
+
+// Logical or Boolean
+console.log("5 == '5' is", 5 == '5');
+console.log("5 === '5' is", 5 === '5');
+
+console.log("5 === 5 is", 5 === 5);
+console.log("5 === 6 is", 5 === 6);
+
+console.log("5 !== 5 is", 5 !== 5);
+console.log("5 !== 6 is", 5 !== 6);
+
+console.log("3 > 2 is", 3 > 2);
+console.log("3 < 2 is", 3 < 2);
+
+console.log("3 <= 2 is", 3 <= 2);
+console.log("2 <= 2 is", 2 <= 2);
+
+var age = 19;
+var dollars = 5;
+
+console.log("age >= 18 && dollars >= 20", age >= 18 && dollars >= 20)
+console.log("age >= 18 || dollars >= 20", age >= 18 || dollars >= 20)
+
+var isGravityEnabled = true;
+console.log("isGravityEnabled", isGravityEnabled)
+console.log("! isGravityEnabled", !isGravityEnabled)
+
+console.log("true is", true);
+console.log("!false is", !false);
+
+console.log("!!true is", !!true);
--- javascript/square_area.js
+++ javascript/square_area.js
@@ -1,10 +1,9 @@
-function areaSquare(side)
-{
-    let area = side * side;
-    return area;
-}
- 
-// Driver Code
- 
-    let side = 4;
-    document.write(areaSquare(side));
+function userInput() {return Math.floor(Math.random() * 100);} // do not touch or change this lone!!!
+
+var side = userInput();
+var area = side * side;
+
+console.log("./square_area");
+console.log("Square side is ", side);
+console.log("Therefore");
+console.log("Square area is ", area);
