1350b3e30f5da72e62491e0157c67b724e16fa96
AnnaKachur <kachurann@gmail.com>
2023-01-23 21:50:43 +0100

    Practiced conditionals

---
+++ javascript/seasons_1.js
@@ -0,0 +1,43 @@
+function randomInt(min, max) {return Math.floor(Math.random() * (max - min)) + min;} // do not touch or change this line!!!
+
+var month_index = randomInt(1, 12);
+
+console.log("./seasons");
+console.log("Month index is", month_index);
+
+if (month_index === 1) {
+    console.log("It is January");
+}
+if (month_index === 2) {
+    console.log("It is February");
+}
+if (month_index === 3) {
+    console.log("It is March");
+}
+if (month_index === 4) {
+    console.log("It is April");
+}
+if (month_index === 5) {
+    console.log("It is May");
+}
+if (month_index === 6) {
+    console.log("It is June");
+}
+if (month_index === 7) {
+    console.log("It is July");
+}
+if (month_index === 8) {
+    console.log("It is August");
+}
+if (month_index === 9) {
+    console.log("It is September");
+}
+if (month_index === 10) {
+    console.log("It is October");
+}
+if (month_index === 11) {
+    console.log("It is November");
+}
+if (month_index === 12) {
+    console.log("It is December");
+}
