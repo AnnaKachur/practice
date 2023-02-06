d7d31c8444361ffaf404a132bac20e8759555080
AnnaKachur <kachurann@gmail.com>
2023-02-06 18:52:33 +0100

    Learning functions

---
+++ javascript/materials/learning_functions.js
@@ -0,0 +1,42 @@
+// functions
+
+function sayHello() {
+    console.log("Hello");
+    
+    // code
+    // if
+    // for
+    // while
+    // function
+}
+
+3 + (2 / (6 + (2 - 4)))
+
+sayHello();
+sayHello();
+let result = sayHello();
+console.log(result);
+
+function celsiusToFarenheit(farenheit) {
+    const celsius = (farenheit - 32) * 5 / 9;
+
+    return celsius;
+}
+
+console.log("In the morning", celsiusToFarenheit(60) + "C");
+console.log("During the day", celsiusToFarenheit(80) + "C");
+
+function sayTwoTimes(text) {
+    console.log(text);
+    console.log(text);
+}
+
+sayTwoTimes("Hello to Jerry!");
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+let add_result = add(3, 5);
+console.log("add result", add_result)
+console.log(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
